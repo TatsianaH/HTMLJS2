6109c02007f4cfcd948a9fd386b0ff7a58bea000
TatsianaH <QA4success>
2019-08-05 15:43:05 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function sayHello(){
+console.log("Say Hello!!!");
+document.body.style.backgroundColor = 'orange';
+document.getElementById('h1').style.backgroundColor = 'green';
+document.getElementById('h1').style.fontSize = '50px';
+document.getElementById('display').value = 'Hello Display';
+}
+function mouseEnter(){
+console.log("Hi!!!");
+}
+function read(){
+return document.getElementById('display').value;
+}
+
+function addToList(){
+let para = document.createElement("li");
+let node = document.createTextNode(read());
+para.appendChild(node);
+
+let element = document.getElementById("list");
+element.appendChild(para);
+}
+//document.body.style.backgroundColor = 'green';
